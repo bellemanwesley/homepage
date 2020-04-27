aab6c90f097be67a81c1422773eb9a28d95589a7
Wesley Belleman <bellemanwesley@gmail.com>
2020-04-27 21:49:24 +0000

    auto commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,16 +1,7 @@
 # coding=utf-8
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-import requests
-import sys
-import urllib
 
 def home(request):
-	return HttpResponseRedirect('/get_content/?cname=home')
-
-def get_content(request):
-	cname = request.GET.get('cname',False)
-	cname = cname.replace("_","/")
-        data = requests.get('https://raw.githubusercontent.com/bellemanwesley/homepage/master/s3_files/'+cname+'_content.html')
-        return(render(request,'home.html',{'data':data.text}))
+	return(render(request,'home.html',{}))
+        
 
